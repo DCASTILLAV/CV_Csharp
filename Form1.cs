db70a54f805acb9d5d93b725e96393200dcd9756
danny castilla <jdannycastilla@outlook.com>
2024-11-19 10:17:39 -0500

    version1.0.5

--- Form1.cs
+++ Form1.cs
@@ -13,8 +13,11 @@ namespace Prueba
         {
             using (WebClient client = new WebClient())
             {
-                string versionInfo = client.DownloadString("");
+                string versionInfo = client.DownloadString("https://github.com/DCASTILLAV/CV_Csharp/tree/master/pubicacion/version.json");
                 // Procesar la información de la versión
+
+                client.DownloadFile("https://github.com/DCASTILLAV/CV_Csharp/tree/master/pubicacion/Designer.exe", "D:/Descargas/");
+                MessageBox.Show("se descargo la aplicacion de forma correcta");
             }
         }
     }
